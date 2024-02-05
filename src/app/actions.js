492e390042d7a3a15c93158edab626f104274a28
0xafrogeek <babouceesay@users.noreply.github.com>
2024-02-05 11:24:36 +0000

    Initial commit: Set up minimal working poc

---
+++ src/app/actions.js
@@ -0,0 +1,21 @@
+'use server'
+
+import axios from 'axios'
+
+const baseUrl = process.env.API_BASE_URL
+
+export default async function getIndicators() {
+    try {
+        const response = await axios.get(`${baseUrl}/indicators`, {
+            headers: {
+                Authorization: 'ApiToken ' + process.env.API_KEY,
+            },
+        })
+        const data = response.data
+        console.log(data)
+        return data
+    } catch (error) {
+        console.error('Error:', error)
+        return { errors: error.message }
+    }
+}
