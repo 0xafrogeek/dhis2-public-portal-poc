103d66c477279d222fa86e0768af8a8e468a9a39
0xafrogeek <babouceesay@users.noreply.github.com>
2024-02-29 16:30:09 +0000

    chore: Change middleware baseUrl

--- src/app/actions.js
+++ src/app/actions.js
@@ -6,7 +6,7 @@ const baseUrl = process.env.API_BASE_URL
 
 export default async function getIndicators() {
     try {
-        const response = await axios.get(`${baseUrl}/indicators`, {
+        const response = await axios.get(`${baseUrl}/api/v1/indicators`, {
             headers: {
                 Authorization: 'ApiToken ' + process.env.API_KEY,
             },
